736851c23e7b18d0459a4f3ff65712481a46d7e2
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2021-05-16 17:13:42 -0400

    setting up enviroment and Alan

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '5ee5f842a3f8c2c879a880b7f203c7732e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key : alanKey,
+            onCommand : ({ command }) => {
+                if(command === 'testCommand'){
+                    alert('This code was executed');
+                }
+            }
+        })
+    }, [])
+    return (
+        <div>
+            <h1>Alan AI News Application</h1>
+        </div>
+    )
+}
+
+export default App;
